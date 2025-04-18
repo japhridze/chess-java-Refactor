ee57cfe3e7c3593ebe9d4e5d0459a6d98c899e75
TORNIKE.JAPARIDZE <tornikejaparidze777@@example.com>
2025-04-18 23:40:11 +0400

    Renamed variable for clarity, Improved method structure and encapsulation.

--- src/model/Square.java
+++ src/model/Square.java
@@ -1,92 +1,89 @@
 package model;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
-
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Square extends JComponent {
-    private Board b;
-    
+
+    private final Board board;
     private final int color;
+    private final int xNum;
+    private final int yNum;
+
     private Piece occupyingPiece;
-    private boolean dispPiece;
-    
-    private int xNum;
-    private int yNum;
-    
-    public Square(Board b, int c, int xNum, int yNum) {
-        
-        this.b = b;
-        this.color = c;
-        this.dispPiece = true;
+    private boolean displayPiece;
+
+    public Square(Board board, int color, int xNum, int yNum) {
+        this.board = board;
+        this.color = color;
         this.xNum = xNum;
         this.yNum = yNum;
-        
-        
-        this.setBorder(BorderFactory.createEmptyBorder());
+        this.displayPiece = true;
+
+        setBorder(BorderFactory.createEmptyBorder());
     }
-    
+
     public int getColor() {
         return this.color;
     }
-    
-    public Piece getOccupyingPiece() {
-        return occupyingPiece;
-    }
-    
-    public boolean isOccupied() {
-        return (this.occupyingPiece != null);
-    }
-    
+
     public int getXNum() {
         return this.xNum;
     }
-    
+
     public int getYNum() {
         return this.yNum;
     }
-    
-    public void setDisplay(boolean v) {
-        this.dispPiece = v;
+
+    public Piece getOccupyingPiece() {
+        return this.occupyingPiece;
     }
-    
-    public void put(Piece p) {
-        this.occupyingPiece = p;
-        p.setPosition(this);
+
+    public boolean isOccupied() {
+        return this.occupyingPiece != null;
     }
-    
+
+    public void setDisplay(boolean display) {
+        this.displayPiece = display;
+    }
+
+    public void put(Piece piece) {
+        this.occupyingPiece = piece;
+        piece.setPosition(this);
+    }
+
     public Piece removePiece() {
-        Piece p = this.occupyingPiece;
+        Piece removed = this.occupyingPiece;
         this.occupyingPiece = null;
-        return p;
+        return removed;
     }
-    
-    public void capture(Piece p) {
-        Piece k = getOccupyingPiece();
-        if (k.getColor() == 0) b.Bpieces.remove(k);
-        if (k.getColor() == 1) b.Wpieces.remove(k);
-        this.occupyingPiece = p;
+
+    public void capture(Piece attacker) {
+        if (occupyingPiece != null) {
+            if (occupyingPiece.getColor() == 0) {
+                board.Bpieces.remove(occupyingPiece);
+            } else {
+                board.Wpieces.remove(occupyingPiece);
+            }
+        }
+        this.occupyingPiece = attacker;
     }
-    
+
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        if (this.color == 1) {
-            g.setColor(new Color(221,192,127));
-        } else {
-            g.setColor(new Color(101,67,33));
-        }
-        
-        g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
-        
-        if(occupyingPiece != null && dispPiece) {
+
+        Color tileColor = (color == 1) ? new Color(221, 192, 127) : new Color(101, 67, 33);
+        g.setColor(tileColor);
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
+
+        if (occupyingPiece != null && displayPiece) {
             occupyingPiece.draw(g);
         }
     }
-    
+
     @Override
     public int hashCode() {
         int prime = 31;
@@ -95,5 +92,4 @@ public class Square extends JComponent {
         result = prime * result + yNum;
         return result;
     }
-    
 }
