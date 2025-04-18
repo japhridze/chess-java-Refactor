818fe91e62c582027fad9a1abcddfcbabfb71bfc
TORNIKE.JAPARIDZE <tornikejaparidze777@@example.com>
2025-04-18 23:45:09 +0400

    refactor GameWindow.java Extracted timer and button logic into helper methods, educed code duplication in checkmate/time handling

--- src/view/Clock.java
+++ src/view/Clock.java
@@ -1,36 +1,34 @@
 package view;
 
 public class Clock {
-    private int hh;
-    private int mm;
-    private int ss;
+    private int hours;
+    private int minutes;
+    private int seconds;
     
-    public Clock(int hh, int mm, int ss) {
-        this.hh = hh;
-        this.mm = mm;
-        this.ss = ss;
+    public Clock(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
     }
     
     public boolean outOfTime() {
-        return (hh == 0 && mm == 0 && ss == 0);
+        return (hours == 0 && minutes == 0 && seconds == 0);
     }
     
     public void decr() {
-        if (this.mm == 0 && this.ss == 0) {
-            this.ss = 59;
-            this.mm = 59;
-            this.hh--;
-        } else if (this.ss == 0) {
-            this.ss = 59;
-            this.mm--;
-        } else this.ss--;
+        if (this.minutes == 0 && this.seconds == 0) {
+            this.seconds = 59;
+            this.minutes = 59;
+            this.hours--;
+        } else if (this.seconds == 0) {
+            this.seconds = 59;
+            this.minutes--;
+        } else this.seconds--;
     }
-    
+
+
+//simlified getTime method
     public String getTime() {
-        String fHrs = String.format("%02d", this.hh);
-        String fMins = String.format("%02d", this.mm);
-        String fSecs = String.format("%02d", this.ss);
-        String fTime = fHrs + ":" + fMins + ":" + fSecs;
-        return fTime;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
